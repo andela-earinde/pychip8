62048571528f667b6d2aee6e8b27acc6adbfdd10
Arinde Eniola <eniola.arinde@andela.co>
2015-10-24 17:05:15 +0100

    set up directory and setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+
+setup(
+    name='pychip8',
+    version='0.0.1',
+    description='Implementing the chip8 interpreter in python',
+    long_description=readme,
+    author='Arinde Eniola',
+    author_email='eniola.arinde@andela.com',
+    url='https://github.com/andela-earinde/pychip8',
+    license='MIT',
+    packages=find_packages(exclude=('tests', 'docs'))
+)
